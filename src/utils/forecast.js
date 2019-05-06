9a1beddd324b2324450a8bd43815ddefdbb798e1
Matheus <Binotto>
2019-05-06 13:24:41 -0300

    Upgrade

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. This a high today is ' + body.daily.data[0].temperatureHigh + ' with a low of ' + body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + Math.round((body.currently.temperature - 32) * 5/9) + ' degress out. This a high today is ' + Math.round((body.daily.data[0].temperatureHigh - 32) * 5/9) + ' with a low of ' + Math.round((body.daily.data[0].temperatureLow - 32) * 5/9) + '. There is a ' + body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
